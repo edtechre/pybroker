368f0928df4edc0369293916f03b37d3278dea68
edtechre <edtechre@gmail.com>
2023-01-23 22:31:24 -0800

    Clean up import.

--- src/pybroker/strategy.py
+++ src/pybroker/strategy.py
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License along
 with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-from pybroker.cache import CacheDateFields
+from .cache import CacheDateFields
 from .common import (
     BarData,
     DataCol,
