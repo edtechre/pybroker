8158a115bb1b0ab40106f9cc0d8e92712232fe6d
edtechre <edtechre@gmail.com>
2023-06-24 17:56:31 -0700

    Update license comment in source files.

--- src/pybroker/indicator.py
+++ src/pybroker/indicator.py
@@ -1,18 +1,9 @@
 """Contains indicator related functionality."""
 
-"""Copyright (C) 2023 Edward West
+"""Copyright (C) 2023 Edward West. All rights reserved.
 
-This program is free software: you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free
-Software Foundation, either version 3 of the License, or (at your option) any
-later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License along
-with this program.  If not, see <https://www.gnu.org/licenses/>.
+This code is licensed under Apache 2.0 with Commons Clause license
+(see LICENSE for details).
 """
 
 import functools
