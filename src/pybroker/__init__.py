b3008e939d343785588c386eed8dbdb9a54b935a
edtechre <edtechre@gmail.com>
2024-03-16 20:38:46 -0700

    Update dev version to v1.1.33

--- src/pybroker/__init__.py
+++ src/pybroker/__init__.py
@@ -50,4 +50,4 @@ from pybroker.vect import cross, highv, lowv, returnv, sumv
 # https://github.com/numba/numba/issues/8940
 from numba.np.unsafe import ndarray
 
-__version__ = "1.1.32"
+__version__ = "1.1.33"
